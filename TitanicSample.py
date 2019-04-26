b90cf51eefee82aeec391d00845d51029103ee8d
sagar-adglobal <sagar.gupta@adglobal360.com>
2019-04-26 16:33:02 +0530

    In Progress

--- TitanicSample.py
+++ TitanicSample.py
@@ -10,10 +10,11 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('/home/agl-android/.ML/Titanic/data/train.csv')
 
-x = df[['PassengerId','Pclass','Sex','Age']].values
+x = df[['PassengerId','Pclass','Sex','Age','SibSp','Parch']].values
 y = df[['Survived']].values
 
 imputer = SimpleImputer(strategy="mean")
@@ -22,3 +23,22 @@ x[:, 3:4] = imputer.fit_transform(x[:, 3:4]).astype(dtype= int)
 labelEncoder_X = LabelEncoder()
 x[:, 2] = labelEncoder_X.fit_transform(x[:, 2])
 
+dfTest = pd.read_csv('/home/agl-android/.ML/Titanic/data/test.csv')
+xTest =  dfTest[['PassengerId','Pclass','Sex','Age','SibSp','Parch']].values
+
+imputerTest = SimpleImputer(strategy="mean")
+xTest[:, 3:4] = imputerTest.fit_transform(xTest[:, 3:4]).astype(dtype= int)
+
+labelEncoder_XTest = LabelEncoder()
+xTest[:, 2] = labelEncoder_XTest.fit_transform(xTest[:, 2])
+
+regr = LinearRegression()
+regr = regr.fit(x,y)
+yPridict = regr.predict(xTest).astype(dtype = int)
+
+out = pd.DataFrame()
+out['PassengerId'] = xTest[:,0]
+out['Survived'] = yPridict
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(yPridict, y)
